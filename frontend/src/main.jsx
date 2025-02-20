0959ab69f583769e1a0e74f12847207f7fc17781
Dimitar Gocev <dimitar.gocev@students.finki.ukim.mk>
2025-02-20 18:35:18 +0100

    Began login/register frontend implementation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from './pages/home/Home.jsx';
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
 
@@ -13,7 +15,10 @@ createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Home />} />
         <Route path="/orders" element={<h1>Orders</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Route>
+      
     </Routes>
   </BrowserRouter>
 )
